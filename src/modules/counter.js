ba8ccd759341061f923518bb530b2e0fa51df4d9
Jon Szeto <jonszeto821@gmail.com>
2018-05-16 17:22:55 -0400

    create getFoodItems function to find food items with brand id & remove functions not in use

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,70 +1,33 @@
-export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED';
-export const INCREMENT = 'counter/INCREMENT';
-export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED';
-export const DECREMENT = 'counter/DECREMENT';
-
 const initialState = {
   count: 0,
-  isIncrementing: false,
-  isDecrementing: false,
   name: 'Jon',
   strain: '',
-  foods: []
+  foods: [],
+  brands: [],
+  brand: { fields: { name: null, _id: null } }
 };
 
 export default (state = initialState, action) => {
   // console.log(action);
   switch (action.type) {
-    case INCREMENT_REQUESTED:
-      return {
-        ...state,
-        isIncrementing: true
-      };
-
-    case INCREMENT:
-      return {
-        ...state,
-        count: state.count + 1,
-        isIncrementing: !state.isIncrementing
-      };
-
-    case DECREMENT_REQUESTED:
-      return {
-        ...state,
-        isDecrementing: true
-      };
-
-    case DECREMENT:
-      return {
-        ...state,
-        count: state.count - 1,
-        isDecrementing: !state.isDecrementing
-      };
-
     case 'CHANGENAME':
       return {
         ...state,
         name: 'Szeto',
-        foods: action.payload.hits
+        brands: action.payload.hits
+      };
+
+    case 'SET_BRAND':
+      return {
+        ...state,
+        brand: action.payload
       };
 
     default:
       return state;
   }
 };
-
-export const increment = () => {
-  return dispatch => {
-    dispatch({
-      type: INCREMENT_REQUESTED
-    });
-
-    dispatch({
-      type: INCREMENT
-    });
-  };
-};
-
+// used to search for restaurants based on search
 export const submit = values => {
   console.log(values, this);
   return dispatch => {
@@ -97,102 +60,47 @@ export const submit = values => {
   };
 };
 
-export const exercise = () => {
-  return dispatch => {
-    let url = 'https://www.nutritionix.com/track-api/v2/natural/exercise';
-    let data = {
-      query: 'ran 3 miles',
-      gender: 'female',
-      weight_kg: 72.5,
-      height_cm: 167.64,
-      age: 30
-    };
-
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-        // 'x-app-id':'a49ed4c7',
-        // 'x-app-key':'393944159c41c173ad29ff4a1d08ad97',
-        // 'Accept': 'application/json, text/plain, /',
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Credentials": "true",
-        // "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-        // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-      })
-    })
-      .then(res => res.json())
-      .catch(error => console.error('Error:', error))
-      .then(response => console.log('Success:', response));
-  };
-};
-
-export const incrementAsync = () => {
+// used to select and choose which restaurant from list
+export const setBrand = value => {
+  console.log(value, this);
   return dispatch => {
     dispatch({
-      type: INCREMENT_REQUESTED
-    });
-
-    return setTimeout(() => {
-      dispatch({
-        type: INCREMENT
-      });
-    }, 3000);
-  };
-};
-
-export const decrement = () => {
-  return dispatch => {
-    dispatch({
-      type: DECREMENT_REQUESTED
-    });
-
-    dispatch({
-      type: DECREMENT
+      type: 'SET_BRAND',
+      payload: value
     });
   };
 };
 
-export const decrementAsync = () => {
-  return dispatch => {
-    dispatch({
-      type: DECREMENT_REQUESTED
-    });
-
-    return setTimeout(() => {
-      dispatch({
-        type: DECREMENT
-      });
-    }, 3000);
-  };
-};
-
-export const submitMacro = values => {
+// define the calorie params
+export const setCals = values => {
   console.log(values, this);
   return dispatch => {
-    let url = 'https://api.nutritionix.com/v1_1/search';
+    dispatch({
+      type: 'SET_CALS',
+      payload: values
+    });
+  };
+};
+
+// find food items with brand id & cal params
+export const getFoodItems = value => {
+  console.log(value, this);
+  return dispatch => {
+    let url = new URL(`https://api.nutritionix.com/v1_1/search/`);
     let data = {
-      // "brand_id":'513fbc1283aa2dc80c00055c',
-      query: 'dominos',
-      cal_min: 300,
-      cal_max: 900,
+      brand_id: value._id,
+      results: '0:50',
+      cal_min: 50,
+      cal_max: 280,
+      fields: '*',
       appId: 'a49ed4c7',
       appKey: '393944159c41c173ad29ff4a1d08ad97'
     };
+
+    Object.keys(data).forEach(key => url.searchParams.append(key, data[key]));
+    console.log(url);
     fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(data),
-      headers: new Headers({
-        'Content-Type': 'application/json'
-        // 'x-app-id':'a49ed4c7',
-        // 'x-app-key':'393944159c41c173ad29ff4a1d08ad97',
-        // 'Accept': 'application/json, text/plain, /',
-        // "Access-Control-Allow-Origin": "*",
-        // "Access-Control-Allow-Credentials": "true",
-        // "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
-        // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-      })
+      method: 'GET'
     })
       .then(function(response) {
         console.log(response);
@@ -201,10 +109,7 @@ export const submitMacro = values => {
       .then(function(myJson) {
         console.log(myJson);
 
-        // dispatch({
-        //   type: 'CHANGENAME',
-        //   payload: myJson,
-        //   });
+        dispatch({});
       });
   };
 };
