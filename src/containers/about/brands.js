a770df5ac7a0baaabbe5b67bbc145594cfe5c7ff
Jon Szeto <jonszeto821@gmail.com>
2018-05-16 17:18:42 -0400

    rename foods.js to brands.js

---
+++ src/containers/about/brands.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Brand = props => {
+  console.log(props);
+  let brands = props.brands.map((brand, i) => {
+    return (
+      <li
+        onClick={() => {
+          props.setBrand(brand);
+        }}
+        key={i}>
+        ID: {brand.fields._id} <br />
+        Restaurant: {brand.fields.name}
+      </li>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Food Page: {props.brand.fields.name}</h1>
+      <ul>{brands}</ul>
+    </div>
+  );
+};
+
+export default Brand;
+
+// function NumberList(props) {
+//   const numbers = props.numbers;
+//   const listItems = numbers.map((number) =>
+//     <li>{number}</li>
+//   );
+//   return (
+//     <ul>{listItems}</ul>
+//   );
+// }
