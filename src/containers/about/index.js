90ca442dedb8befa8faa2e0410795c194039ac69
Jon Szeto <jonszeto821@gmail.com>
2018-06-15 14:14:17 -0400

    add description and purpose of app

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 const About = () => (
   <div>
-    <h1>About Page</h1>
-    <p>Did you get here via Redux?</p>
+    <h1>About</h1>
+    <h3>Purpose:</h3>
+    <p>
+      The purpose of the application is to provide users to search nutrition
+      information based on restaurant and calorie range inputs selected by the
+      user.
+    </p>
+
+    <h3>Features:</h3>
+    <ul>
+      <li>Search for a restaurant</li>
+      <li>Define calorie range</li>
+      <li>Filter menu items based on macros (fats, carbs, protein)</li>
+      <li>Add food items to list to review</li>
+      <li>Remove food items from review list</li>
+    </ul>
   </div>
 );
 
